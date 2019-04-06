f3737d8065fadbe099d465e5f42900b940e5d632
Anderson Cossul <anderson_cossul@hotmail.com>
2019-04-06 11:10:20 -0300

    Migrado para androidx

--- app/src/main/java/cossul/anderson/marvelcharacters/activities/MainActivity.kt
+++ app/src/main/java/cossul/anderson/marvelcharacters/activities/MainActivity.kt
@@ -1,6 +1,6 @@
 package cossul.anderson.marvelcharacters.activities
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cossul.anderson.marvelcharacters.R
 
