9140f80201f69b2e1d0043841232b0e3130dbeb2
Anderson Cossul <anderson_cossul@hotmail.com>
2019-04-07 22:20:23 -0300

    readicionado testes não implementados

--- app/src/test/java/cossul/anderson/marvelcharacters/network/MarvelAPITest.kt
+++ app/src/test/java/cossul/anderson/marvelcharacters/network/MarvelAPITest.kt
@@ -1,6 +1,5 @@
 package cossul.anderson.marvelcharacters.network
 
-import cossul.anderson.marvelcharacters.network.MarvelAPI
 import org.junit.Test
 
 import org.junit.runner.RunWith
---
+++ app/src/test/java/cossul/anderson/marvelcharacters/viewmodels/ComicsViewModelTest.kt
@@ -0,0 +1,13 @@
+package cossul.anderson.marvelcharacters.viewmodels
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ComicsViewModelTest {
+
+    @Test
+    fun getComics() {
+        // test observing comics liveData change
+    }
+}
